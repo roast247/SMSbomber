bd02a2699d1f9394f2055b0f81bcc377e87978b2
roast247 <77567356+roast247@users.noreply.github.com>
2021-03-17 00:04:19 -0700

    Add files via upload

---
+++ Splash.vb
@@ -0,0 +1,13 @@
+﻿Public Class Splash
+    Private Sub Splash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(8)
+        If ProgressBar1.Value = ProgressBar1.Maximum Then
+            Me.Hide()
+            SMSBomber.Show()
+            Timer1.Stop()
+        End If
+    End Sub
+End Class
